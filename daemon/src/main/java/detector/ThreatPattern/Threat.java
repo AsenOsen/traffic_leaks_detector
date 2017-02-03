e37f9c155906db0d6b0702dfa6ebb0879b28e83c
Asen <antochi.anton@yandex.ru>
2017-02-03 15:12:38 +0300

    Little refactoring and reorganization

--- daemon/src/main/java/detector/ThreatPattern/Threat.java
+++ daemon/src/main/java/detector/ThreatPattern/Threat.java
@@ -14,18 +14,18 @@ import java.util.Date;
 public class Threat
 {
 
-    private NetProcess process;   // process which caused the getThreatMessage
-    private IPv4Address ip;       // ip which caused the getThreatMessage
-    private Port port;            // port which caused the getThreatMessage
-    private TrafficFlow traffic;  // malicious traffic
+    private NetProcess thrProcess;   // process which caused the getThreatMessage
+    private IPv4Address thrIp;       // ip which caused the getThreatMessage
+    private Port thrPort;            // port which caused the getThreatMessage
+    private TrafficFlow thrTraffic;  // malicious traffic
 
 
     public Threat(TrafficFlow traffic)
     {
-        this.process = traffic.getDominantProcess();
-        this.ip = traffic.getDominantDstAddr();
-        this.port = traffic.getDominantSrcPort();
-        this.traffic = traffic;
+        this.thrProcess = traffic.getDominantProcess();
+        this.thrIp = traffic.getDominantDstAddr();
+        this.thrPort = traffic.getDominantSrcPort();
+        this.thrTraffic = traffic;
     }
 
 
@@ -40,6 +40,7 @@ public class Threat
         }
 
         // if no pattern match found
+        assert false : "Impossible situation detected!";
         ThreatMessage report = new ThreatMessage();
         report.setUserMessage(this.toString());
         return report;
@@ -48,27 +49,27 @@ public class Threat
 
     public NetProcess getInitiatorProcess()
     {
-        return process;
+        return thrProcess;
     }
 
     public IPv4Address getForeignIp()
     {
-        return ip;
+        return thrIp;
     }
 
     public Port getInitiatorPort()
     {
-        return port;
+        return thrPort;
     }
 
     public int getLeakSizeBytes()
     {
-        return traffic.getBytes();
+        return thrTraffic.getBytes();
     }
 
     public float getActivityTime()
     {
-        return traffic.getLifeTimeSec();
+        return thrTraffic.getLifeTimeSec();
     }
 
 
@@ -78,14 +79,14 @@ public class Threat
         StringBuilder msg = new StringBuilder();
         msg.append("Time: "+(new Date().toString())+"\n");
 
-        if(process != null)
-            msg.append("[process]\n"+process+" => "+ traffic+"\n");
+        if(thrProcess != null)
+            msg.append("[process]\n"+ thrProcess +" => "+ thrTraffic +"\n");
         else
-        if(ip != null)
-            msg.append("[ip]\n"+ip + " <= " + traffic+"\n");
+        if(thrIp != null)
+            msg.append("[ip]\n"+ thrIp + " <= " + thrTraffic +"\n");
         else
-        if(port != null)
-            msg.append("[src port]\n:"+port + " => " + traffic+"\n");
+        if(thrPort != null)
+            msg.append("[port]\n:"+ thrPort + " => " + thrTraffic +"\n");
 
         return msg.toString();
     }
